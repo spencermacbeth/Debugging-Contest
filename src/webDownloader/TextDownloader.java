8c6bcbf4119c58394dd38369a1da0202a401bcdc
Spencer MacBeth <Spencer MacBeth>
2016-04-28 16:34:05 -0700

    Commit first

---
+++ src/webDownloader/TextDownloader.java
@@ -0,0 +1,12 @@
+package webDownloader;
+
+public class TextDownloader extends WebDownloader {
+	
+	public TextDownloader(String w, String d, String fr) {
+		fileExtension = ".txt";
+		website = w;
+		destination = d;
+		fileRoot = fr;
+	}
+
+}
